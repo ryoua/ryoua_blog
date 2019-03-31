eb08a56caa8eea01f37806bde43a5de137e88149
徐炳华 <31928820+ryoua@users.noreply.github.com>
2019-03-31 15:10:43 +0800

    update

---
+++ src/main/java/com/ryoua/blog/Application.java
@@ -0,0 +1,14 @@
+package com.ryoua.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author ryoua Created on 2019/3/30
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
