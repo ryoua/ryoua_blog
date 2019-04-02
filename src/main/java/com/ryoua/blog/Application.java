4b2943482c22271a1272323e4cab3124cdd0bc9a
ryoua <ryoua@ryouadeiMac.local>
2019-04-02 12:15:43 +0800

    update on mac

--- src/main/java/com/ryoua/blog/Application.java
+++ src/main/java/com/ryoua/blog/Application.java
@@ -2,11 +2,13 @@ package com.ryoua.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * @Author ryoua Created on 2019/3/30
  */
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
