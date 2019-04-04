308f57eab9d5215494825258b7113ee7271307ff
ryoua <ryoua@ryouadeiMac.local>
2019-04-04 12:34:20 +0800

    update

--- src/main/java/com/ryoua/blog/controller/MainController.java
+++ src/main/java/com/ryoua/blog/controller/MainController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-
     @GetMapping("/")
     public String root() {
         return "redirect:/index";
