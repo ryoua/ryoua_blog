545aa63f9b81a2dfb3280241cc3323bea7ad3403
ryoua <1794970183@qq.com>
2019-04-26 15:03:00 +0800

    update

--- src/main/java/com/ryoua/spring/boot/blog/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.ryoua.spring.boot.blog.service;
-
-/**
- * @Author ryoua Created on 2019-04-24
- */
-public interface UserService {
-    boolean isUserExist(String username, String password);
-}
